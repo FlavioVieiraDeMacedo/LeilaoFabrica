fb2d0df72385b70be932efe0e92eb1fd2df449d3
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-07 08:44:54 -0200

    Fields no UsuarioViewModel

--- Fiap.Leilao.Web/ViewModels/UsuarioViewModel.cs
+++ Fiap.Leilao.Web/ViewModels/UsuarioViewModel.cs
@@ -10,7 +10,16 @@ namespace Fiap.Leilao.Web.ViewModels
 
         #region FIELDS
 
-
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Cpf { get; set; }
+        public string Cep { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
 
         #endregion
 
