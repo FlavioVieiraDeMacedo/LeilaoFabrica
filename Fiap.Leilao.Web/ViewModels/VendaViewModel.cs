f99cae6254e5f504d567af62013ba617955b1cf1
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-07 10:41:41 -0200

    Added VendaViewModel

---
+++ Fiap.Leilao.Web/ViewModels/VendaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Fiap.Leilao.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Leilao.Web.ViewModels
+{
+    public class VendaViewModel
+    {
+
+        #region FEILDs
+        public ICollection<Produto> Produtos { get; set; }
+        #endregion
+    }
+}
