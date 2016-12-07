f951460a2181c2f0da147de68d33df936560c891
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-07 08:41:12 -0200

    Added UsuarioController, UsuarioViewModel

---
+++ Fiap.Leilao.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Leilao.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        #region GETS
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region POSTS
+        [HttpPost]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ Fiap.Leilao.Web/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Leilao.Web.ViewModels
+{
+    public class UsuarioViewModel
+    {
+
+        #region FIELDS
+
+
+
+        #endregion
+
+    }
+}
