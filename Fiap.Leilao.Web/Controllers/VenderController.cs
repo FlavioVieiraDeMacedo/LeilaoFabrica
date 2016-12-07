e1b770556f0f27561d9d758eb5dbf2636043a939
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-07 10:26:37 -0200

    Added VenderController

---
+++ Fiap.Leilao.Web/Controllers/VenderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Leilao.Web.Controllers
+{
+    public class VenderController : Controller
+    {
+        // GET: Vender
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
